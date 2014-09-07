af5f165d11928a8e55379f1443d45ecb5af57570
mhuerster <mary.huerster@gmail.com>
2014-09-07 15:00:06 -0400

    create method to make http request see comment for rough approximation of http request to use will use options hash and string interpolation to customize request

--- lib/fauxster.rb
+++ lib/fauxster.rb
@@ -2,7 +2,13 @@ require "fauxster/version"
 require "httparty"
 
 module Fauxster
+
   def self.say_stuff
   	puts "hello world"
+  end
+
+  def self.craft_prose(args = {})
+  	# going to use something like this
+  	# HTTParty.get('http://hipsterjesus.com/api/?paras=1&html=false'&type='hipster-centric')
   end
 end
