4e3f5c34bdf1d2507e5bea2ff92c5a10036c697a
mhuerster <mary.huerster@gmail.com>
2014-09-08 16:07:47 -0400

    change hash keys from strings to symbols easier for user to input also added TODO comment - error handling

--- lib/fauxster.rb
+++ lib/fauxster.rb
@@ -4,8 +4,9 @@ require "httparty"
 module Fauxster
 
   def self.craft_prose(paragraphs = 1, args = {})
-  	html = args.fetch('html') { false }
-  	language = args.fetch('language') { 'hipster-centric' }
+    # TODO: add error handling
+  	html = args.fetch(:html) { false }
+  	language = args.fetch(:language) { 'hipster-centric' }
   	
   	hipster_nonsense = HTTParty.get("http://hipsterjesus.com/api/?paras=#{paragraphs}&html=#{html}&type=#{language}")
     return hipster_nonsense['text']
