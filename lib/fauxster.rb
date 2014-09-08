b8e8a7449adf0196318b6db0ba7d3c69082805ab
mhuerster <mary.huerster@gmail.com>
2014-09-08 15:55:26 -0400

    remove comments

--- lib/fauxster.rb
+++ lib/fauxster.rb
@@ -7,10 +7,11 @@ module Fauxster
   	puts "hello world"
   end
 
-  def self.craft_prose(args = {})
-  	paragraphs = args.fetch('paras') { 1 }
+  def self.craft_prose(paragraphs = 1, args = {})
+  	
   	html = args.fetch('html') { false }
   	language = args.fetch('language') { 'hipster-centric' }
+  	
   	# going to use something like this
   	hipster_nonsense = HTTParty.get("http://hipsterjesus.com/api/?paras=#{paragraphs}&html=#{html}&type=#{language}")
     return hipster_nonsense['text']
